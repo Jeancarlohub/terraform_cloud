5200b91d72efbda492a9fea9310868d1c35c4e35
Jeancarlo <jeancarloteranromero@gmail.com>
2026-08-10 20:48:06 -0500

    primer commit

---
+++ provider.tf
@@ -0,0 +1,16 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "b497fd69-266c-46a9-b55b-8be0cd579667"
+}
---
+++ resource_group.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg-01" {
+  name     = "rg-jean-dev-westeurope-01"
+  location = "West Europe"
+}
