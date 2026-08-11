edc2dece417966d889f37a9640942a0caddc42dd
Jeancarlo <jeancarloteranromero@gmail.com>
2026-08-10 21:06:32 -0500

    segundo commit

--- resource_group.tf
+++ resource_group.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rg-01" {
   name     = "rg-jean-dev-westeurope-01"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "rg-02" {
+  name     = "rg-jean-dev-westeurope-02"
+  location = "West Europe"
+}
